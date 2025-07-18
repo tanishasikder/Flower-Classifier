63d4e82de6718a29da5f6f6ef477c9569c0c23c2
tanisha <thetanishasikder@gmail.com>
2025-07-17 23:21:42 -0400

    Improved model efficiency

--- flower_classification.py
+++ flower_classification.py
@@ -8,23 +8,27 @@ import matplotlib.pyplot as plt
 dataset, info = tfds.load('tf_flowers', with_info=True, as_supervised=True)
 full_dataset = dataset['train']
 
-# Training size is 80% of the dataset
+# Training size is 70% of the dataset
 total = info.splits['train'].num_examples
-train_size = int(0.8 * total)
+train_size = int(0.7 * total)
+val_size = int(0.15 * total)
 
 # Train and test sets
+full_dataset = full_dataset.shuffle(total, seed=42)
 train = full_dataset.take(train_size)
-test = full_dataset.skip(train_size)
+validation = full_dataset.skip(train_size).take(val_size)
+test = full_dataset.skip(train_size + val_size)
 
 num_classes = info.features['label'].num_classes
 
-# Resizing the image to 300, 300 and normalizing
+# Resizing the image to 150, 150 and normalizing
 def preprocess(image, label):
-    image = tf.image.resize(image, [300, 300])
+    image = tf.image.resize(image, [150, 150])
     image = tf.cast(image, tf.float32) / 255.0
     return image, label
 
 train = train.map(preprocess)
+validation = validation.map(preprocess)
 test = test.map(preprocess)
 
 # Transforming the data
@@ -35,18 +39,22 @@ data_augmentation = tf.keras.Sequential([
     layers.RandomContrast(0.2)
 ])
 
-train = train.map(lambda x, y: (data_augmentation(x, training=True), y))
-
 SHUFFLE_BUFFER_SIZE = 1000
 PREFETCH_BUFFER_SIZE = tf.data.AUTOTUNE
 BATCH_SIZE = 32
 
 # Final training and testing datasets with parameters
 train_dataset = (train 
+                 .map(lambda x, y: (data_augmentation(x, training=True), y))
                  .cache()
                  .shuffle(SHUFFLE_BUFFER_SIZE)
                  .batch(BATCH_SIZE)
                  .prefetch(PREFETCH_BUFFER_SIZE))
+
+val_dataset = (validation
+               .cache()
+               .batch(BATCH_SIZE)
+               .prefetch(PREFETCH_BUFFER_SIZE))
 
 test_dataset = (test
                 .cache()
@@ -55,12 +63,15 @@ test_dataset = (test
 
 # Building the model with convolutions and max pooling
 model = tf.keras.models.Sequential([
-    tf.keras.Input(shape=(300, 300, 3)),
+    tf.keras.Input(shape=(150, 150, 3)),
     tf.keras.layers.Conv2D(16, (3, 3), activation='relu'),
+    layers.BatchNormalization(),
     tf.keras.layers.MaxPooling2D(2, 2),
     tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    layers.BatchNormalization(),
     tf.keras.layers.MaxPooling2D(2,2),
     tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    layers.BatchNormalization(),
     tf.keras.layers.MaxPooling2D(2,2),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(512, activation='relu'),
@@ -68,30 +79,34 @@ model = tf.keras.models.Sequential([
 ])
 
 # Callback method. Stops at 90% accuracy
-class callback(tf.keras.callbacks.Callback):
+class StoppingCallback(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs={}):
         if (logs.get('accuracy') >= 0.9):
             self.model.stop_training = True
 
-callbacks = callback()
+callbacks = StoppingCallback()
 
 # Compiling the model with a loss, optimizer, and metric
-model.compile(loss='sparse_categorical_crossentropy',
+model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(label_smoothing=0.01),
               optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
               metrics=['accuracy'])
 
 # Model history for the training dataset with 15 epochs and the callback
 history = model.fit(
     train_dataset,
+    validation_data = val_dataset,
     epochs=15,
     verbose=2,
     callbacks=[callbacks]
 )
 
-accuracy = history.history['accuracy']
+train_accuracy = history.history['accuracy']
+val_accuracy = history.history['val_accuracy']
+
 
 # Plotting the accuracy
-plt.plot(accuracy, label='Training Accuracy')
+plt.plot(train_accuracy, label='Training Accuracy')
+plt.plot(val_accuracy, label='Validation Accuracy')
 plt.title('Training Accuracy')
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
@@ -100,3 +115,5 @@ plt.show()
 
 # Evaluating on the test set
 model.evaluate(test_dataset)
+
+model.save('flower_classifier_model')
